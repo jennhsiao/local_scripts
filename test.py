df0e7189bbec3da439ee4707c48499bd213a6441
Jennifer Hsiao <etismeimet@gmail.com>
2020-04-14 14:35:44 -0700

    second commit

--- test.py
+++ test.py
@@ -1,14 +1,10 @@
-import math
-import sys
-import os
 import requests
-
-print(sys.executable)
 
 
 def greet(who_to_greet):
     greeting = 'Hello, {}'.format(who_to_greet)
     return greeting
+
 
 r = requests.get('https://coreyms.com')
 print(r.status_code)
